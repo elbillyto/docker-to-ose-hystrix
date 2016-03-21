23ff47c5d3254503cf66d2cf29f1255f9923e50e
sigreen <sigreen@redhat.com>
2016-03-21 16:37:10 -0400

    second commit

---
+++ src/main/java/hello/CommandHelloWorld.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+/**
+ * The obligatory "Hello World!" showing a simple implementation of a {@link HystrixCommand}.
+ */
+public class CommandHelloWorld extends HystrixCommand<String> {
+
+    private final String name;
+
+    protected CommandHelloWorld(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("SpringBootGroup"));
+        this.name = name;
+    }
+
+    @Override
+    protected String run() throws Exception{
+        return "Hello " + name + "!";
+    }    
+
+}
